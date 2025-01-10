b9c68868968ea021839f0fca81c8920b3f2dce0f
asadchafi <42813770+asadchafi@users.noreply.github.com>
2025-01-10 11:24:36 +0100

    Semi-Auto treasure hunt

--- README.md
+++
@@ -1,2 +0,0 @@
-# treasure-hunt-dofus
- 
--- tracker.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-
-def print_mouse_position():
-    # Get the current mouse position
-    x, y = pyautogui.position()
-    print(f"Mouse position: x={x}, y={y}")
-
-# Call the function
-print_mouse_position()
